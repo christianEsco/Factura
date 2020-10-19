8cd055ed1fb4f67f3ab6fe7d04c85d83c266d20f
Christian Escobar <caescobarl@alumno.edu.gt>
2020-10-18 22:07:10 -0600

    Parcial Iniciar

---
+++ src/parcialfactura/ParcialFactura.java
@@ -0,0 +1,17 @@
+
+package parcialfactura;
+
+/**
+ *
+ * @Christian Alvaro Esciobar Lacan ---- 2000224
+ */
+public class ParcialFactura {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
